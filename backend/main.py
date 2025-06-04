3eab3c6db5d79210974196bab2925696bf5e6742
Oguama <oguamaprosperity@gmail.com>
2025-06-04 03:03:44 +0300

    new url

--- backend/main.py
+++ backend/main.py
@@ -46,7 +46,8 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
-        "https://precision-skin-insights.vercel.app"  # Add your Vercel domain
+        "https://precision-skin-insights.vercel.app",
+        "https://precision-health-bmudwqvyk-pnerrbbys-projects.vercel.app"  # Add your Vercel domain
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
